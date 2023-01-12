84591bf2cc439e7a48465227ab5752dfe9a54d3d
SohIsa <guerra-isa@hotmail.com>
2023-01-12 11:28:34 -0300

    arrays

---
+++ repeticaoEArrays/Multidi.java
@@ -0,0 +1,11 @@
+package repeticaoEArrays;
+
+//Gere e imprima uma matriz 4x4 com valores aleatorios(o a 9)
+
+public class Multidi {
+    
+    public static void main(String[] args) {
+        
+    }
+
+}
