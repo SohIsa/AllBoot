d6855e0063c722c1e7777c7d139521da59d155fb
SohIsa <guerra-isa@hotmail.com>
2023-01-15 21:22:12 -0300

    Fim de repetição e arrays

--- repeticaoEArrays/Multidi.java
+++ repeticaoEArrays/Multidi.java
@@ -1,11 +1,30 @@
 package repeticaoEArrays;
 
+import java.util.Random;
+
 //Gere e imprima uma matriz 4x4 com valores aleatorios(o a 9)
 
 public class Multidi {
     
     public static void main(String[] args) {
         
+        Random random = new Random();
+        int [] [] M = new int [4] [4]; // declaramos uma matriz me chocando ou apenas esquecida?
+
+        for (int i = 0; i < M.length; i++) {
+            for (int j = 0; j < M[i].length; j++) {
+                M[i][j] = random.nextInt(9);
+            }
+        }
+
+        for (int[] linha : M) { // linha da matriz
+            for (int coluna : linha) { //coluna da linha
+                System.out.print(coluna + " ");
+            }
+            System.out.println(); //para pular a linha 
+        }
+
+
     }
 
 }
