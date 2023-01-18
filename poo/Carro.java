0b8a79902b0f85575d8695e0eeffaf90899bc14f
SohIsa <guerra-isa@hotmail.com>
2023-01-18 09:48:07 -0300

    criando o carro

---
+++ poo/Carro.java
@@ -0,0 +1,43 @@
+package poo;
+
+class Carro {
+  
+  String cor;
+  String modelo;
+  int tanque;
+
+  Carro() {
+    
+  }
+
+  Carro(String cor, String modelo, int tanque) {
+    this.cor= cor;
+    this.modelo = modelo;
+    this.tanque = tanque;
+  }
+  
+  void setCor (String cor){
+    this.cor = cor;
+  }
+  String getCor() {
+    return cor;
+  }
+
+  void setModelo (String modelo) {
+    this.modelo = modelo;
+  }
+  String getModelo() {
+    return modelo;
+  }
+
+  void setTanque (int tanque) {
+    this.tanque = tanque;
+  }
+  int getTanque() {
+    return tanque;
+  }
+  double custoTanque (double valorGasolina) {
+    return tanque*valorGasolina;
+  }
+  
+}
---
+++ poo/ExecCar.java
@@ -0,0 +1,17 @@
+package poo;
+
+public class ExecCar {
+    
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+
+        carro.setCor("azul");
+        carro.setModelo("gol");
+        carro.setTanque(12);
+
+        System.out.println(carro.getCor());
+        System.out.println("o custo do tanque " + carro.custoTanque(2.15));
+
+    }
+
+}
