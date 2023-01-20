13468ba7c0f5a132830f2e60b86b6fd78ee48089
SohIsa <guerra-isa@hotmail.com>
2023-01-19 21:21:03 -0300

    associação

--- poo/trabalho/Gerente.java
+++ poo/trabalho/Gerente.java
@@ -9,4 +9,9 @@ public class Gerente extends Funcionario{
     void metodo2(){
         System.out.println("metodo 2 da classes filha (gerente)");
     }
+// sobre agregação: mesmo que o gerente deixe de existir o funcionario não desaparecerá
+    Funcionario funcionario;
 }
+/* sobre composição: algo atrelado e dependente no qual se deixar de existir ele varre tudo junto consigo
+como a relação entre pessoa e endereço (o endereço só pertence a esse pessoa) 
+    sobre dependencia: um metodo de depende da classe para existir*/
