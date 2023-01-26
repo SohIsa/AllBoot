17cc7e49a795f146a6268c0e386522320d27cf24
SohIsa <guerra-isa@hotmail.com>
2023-01-26 20:10:40 -0300

    ja fazem 79 anos

--- poo/collections/Set/Rainbow.java
+++ poo/collections/Set/Rainbow.java
@@ -25,6 +25,32 @@ public class Rainbow {
 
         System.out.println();
         System.out.println("Exiba as cores na ordem inversa da que foi informada");
+        List<String> rainbowI = new ArrayList<>(rainbowO);
+        System.out.println(rainbowI);
+        Collections.sort(rainbowI, Collections.reverseOrder());
+        System.out.println(rainbowI);
+
+        System.out.println();
+        System.out.println("Exiba todas as cores que começam com a letra 'v'");
+        Iterator<String> iterator = rainbow.iterator();
+        while (iterator.hasNext()) {
+            String c = iterator.next();
+            if (c.startsWith("v")) {
+                System.out.println(c);
+            }
+        }
+        
+        System.out.println();
+        System.out.println("Remova todas as cores que não começam com a letra 'v'");
+        rainbow.removeIf(color -> color.startsWith("v"));
+        System.out.println(rainbow);
+
+        System.out.println();
+        System.out.println("limpe a lista e confirme:");
+        rainbow.clear();
+
+        System.out.println(rainbow.isEmpty());
+
         /* Set<String> rainbowI = new LinkedHashSet<>(rainbow);
         System.out.println(rainbowI); */
     } 
