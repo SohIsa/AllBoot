e55b48fb98624aeee9f61b66bf82947c73c90798
SohIsa <guerra-isa@hotmail.com>
2023-02-12 12:57:42 -0300

    ...

--- poo/collections/Set/LinguagemFavorita.java
+++ poo/collections/Set/LinguagemFavorita.java
@@ -1,9 +1,11 @@
 package poo.collections.Set;
 
-public class LinguagemFavorita {
-    private String nome;
-    private Integer anoDeCriacao;
-    private String ide;
+import java.util.Comparator;
+
+public class LinguagemFavorita implements Comparable<LinguagemFavorita> {
+    public String nome;
+    public Integer anoDeCriacao;
+    public String ide;
 
     public LinguagemFavorita (String nome, Integer anoDeCriacao, String ide) {
         this.nome = nome;
@@ -20,5 +22,34 @@ public class LinguagemFavorita {
     public String getIde() {
         return ide;
     }
+    public String toString() {
+        return 
+        "nome= " + nome +
+        ", ano de criacao= " + anoDeCriacao +
+        ", ide= " + ide + "\n";
+    }
 
+    @Override
+    public int compareTo(LinguagemFavorita linguagemFavorita) {
+        return this.nome.compareTo(linguagemFavorita.nome);
+    }
+}
+
+class CompareIde implements Comparator<LinguagemFavorita> {
+
+    @Override
+    public int compare(LinguagemFavorita o1, LinguagemFavorita o2) {
+        // TODO Auto-generated method stub
+        return o1.ide.compareToIgnoreCase(o2.ide);
+    }
+}
+
+class CompareNomeAno implements Comparator<LinguagemFavorita> {
+
+    @Override
+    public int compare(LinguagemFavorita o1, LinguagemFavorita o2) {
+        return Integer.compare(o1.anoDeCriacao, o2.anoDeCriacao);
+        
+    }
+    
 }
--- poo/collections/Set/MainLinguagemFav.java
+++ poo/collections/Set/MainLinguagemFav.java
@@ -1,10 +1,33 @@
 package poo.collections.Set;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class MainLinguagemFav {
-    public static void MainLinguagemFav (String[] args) {
+    public static void main (String[] args) {
         Set<LinguagemFavorita> rank = new HashSet<>();            
         rank.add(new LinguagemFavorita("Python", 1991, "Pycharm"));
+        rank.add(new LinguagemFavorita("JavaScript", 1995, "Intellij"));
+        rank.add(new LinguagemFavorita("Java", 1991, "VSCode"));
+
+        System.out.println(rank.toString());
+
+        System.out.println("ORDEM DE INSERÇÃO");
+        Set<LinguagemFavorita> rank2 = new LinkedHashSet<>(
+            Arrays.asList(
+                new LinguagemFavorita("Python", 1991, "Pycharm"),
+                new LinguagemFavorita("JavaScript", 1995, "Intellij"),
+                new LinguagemFavorita("Java", 1991, "VSCode")
+            )
+        ); System.out.println(rank2);
+
+        System.out.println("ORDEM NATURAL: NOME");
+        Set<LinguagemFavorita> rank3 = new TreeSet<>(rank);
+        System.out.println(rank3);
+        // for (LinguagemFavorita linguagemFavorit : rank) System.out.println(linguagemFavorit);
+
+        System.out.println("ORDEM NATURAL: IDE");
+        Set<LinguagemFavorita> rank4 = new TreeSet<LinguagemFavorita>(new CompareIde());
+        rank4.addAll(rank); System.out.println(rank4);
     }
 }
