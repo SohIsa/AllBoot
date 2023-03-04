ff97c28534c8b5a6b627b177d3673d1532e6ecd1
SohIsa <guerra-isa@hotmail.com>
2023-03-04 19:55:54 -0300

    primeiro desafio

---
+++ desafio0/src/d0_dominio/Cursos.java
@@ -0,0 +1,11 @@
+package d0_dominio;
+
+/**
+ * Cursos
+ */
+public class Cursos {
+
+    private String titulo;
+    private String descricao;
+    private int cargaHoraria;
+}
---
+++ desafio0/src/d0_dominio/Mentoria.java
@@ -0,0 +1,10 @@
+package d0_dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+    
+    private String titulo;
+    private String descricao;
+    private LocalDate data;
+}
